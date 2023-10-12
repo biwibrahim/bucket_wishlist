6613fa9be8f5c724c4e4f322f83c8ddcc88fbd02
biwibrahim <210460001.mhs@stmikjabar.ac.id>
2023-10-12 14:22:48 +0700

    updated

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Tugas\MSIB LearningX\bucket\venv
+set VIRTUAL_ENV=C:\Tugas\MSIB LearningX\part04\bucket\bucket_wishlist\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
